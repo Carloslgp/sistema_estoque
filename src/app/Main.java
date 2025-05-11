0a84fe29580fa9a7f69eff759cc691d8734c7229
Carlos <carloslgp3585@gmail.com>
2025-05-10 21:01:46 -0300

    Commit de teste

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         MenuInicial menuInicial = new MenuInicial();
         menuInicial.executarMenuInicial();
+        System.out.println("teste novo");
 
 
 
